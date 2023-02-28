a783523eacca1ea7a2fbe30b0ef42497de76f0b9
Ostap Onyshkiv <prostoostap58@gmail.com>
2023-02-28 15:20:12 +0200

    added

---
+++ lab1C/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lab1C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.lab3_1();
+            solution.lab4_1();
+            solution.lab4_2();
+            Console.Read();
+        }
+    }
+}
