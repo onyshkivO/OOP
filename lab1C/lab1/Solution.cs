687ea78a30feac73171b61c71aa20e1caebf95ed
Ostap Onyshkiv <prostoostap58@gmail.com>
2023-03-08 17:09:26 +0200

    added c# solution for lab 2 ,1

---
+++ lab1C/lab1/Solution.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+
+namespace lab1C
+{
+    class Solution
+    {
+        public void lab3_1()
+        {
+            double x;
+            double y;
+            double A;
+            double B;
+
+            Console.Write("x= ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+
+            A = 1 / Math.Abs(x + 2) + 1;
+
+            if (x < 1)
+            {
+                B = 7 * x * x + x - 8;
+            }
+            else
+                if (x > 4)
+            {
+                B = Math.Sqrt(1 + Math.Abs(Math.Pow(Math.Cos(x), 3)));
+            }
+            else
+            {
+                B = Math.Cos((x + 4) / Math.Sqrt(11)) / Math.Sin((x + 4) / Math.Sqrt(11)) + 3;
+            }
+
+            y = A - B;
+            Console.WriteLine($"y = {y}"); ;
+        }
+        public void lab4_1()
+        {
+            int i;
+            double P;
+
+            i = 1;
+            P = 1;
+
+            while (i <= 15)
+            {
+                P *= (Sin(i) * Sin(i) + Cos(1 / i) * Cos(1 / i)) / (i * i);
+                i++;
+            }
+            Console.WriteLine("p1= " + P);
+
+            i = 1;
+            P = 1;
+
+            do
+            {
+                P *= (Sin(i) * Sin(i) + Cos(1 / i) * Cos(1 / i)) / (i * i);
+                i++;
+            } while (i <= 15);
+
+            Console.WriteLine("p2= " + P);
+
+            P = 1;
+            i = 1;
+            for (; i <= 15; i++)
+            {
+                P *= (Sin(i) * Sin(i) + Cos(1 / i) * Cos(1 / i)) / (i * i);
+            }
+
+            Console.WriteLine("p3= " + P);
+
+            P = 1;
+            i = 15;
+            for (; i >= 1; i--)
+            {
+                P *= (Sin(i) * Sin(i) + Cos(1 / i) * Cos(1 / i)) / (i * i);
+            }
+
+            Console.WriteLine("p4= " + P);
+
+        }
+        public void lab4_2() {
+
+            double x, xp, xk, dx, A, B, y;
+
+            Console.Write("xp= ");
+            xp = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.Write("xk= ");
+            xk = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("dx= ");
+            dx = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("|        x |         y |");
+            Console.WriteLine("---------------------------");
+
+            x = xp;
+
+            while (x <= xk)
+            {
+                A = 1 / Abs(x + 2) + 1;
+
+                if (x < 1)
+                {
+                    B = 7 * x * x + x - 8;
+                }
+                else
+                if (x > 4)
+                {
+                    B = Sqrt(1 + Abs(Pow(Cos(x), 3)));
+                }
+                else
+                {
+                    B = Cos((x + 4) / Sqrt(11)) / Sin((x + 4) / Sqrt(11)) + 3;
+                }
+
+                y = A - B;
+                Console.WriteLine("| {0,8:f2} |  {1, 8:f2} |  \n", x, y);
+
+                x += dx;
+            }
+
+            Console.WriteLine("---------------------------");
+        }
+
+
+    }
+}
---
+++ lab1C/lab2/Solution.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1C.lab2
+{
+    class Solution
+    {
+        private  struct StudentStruct {
+            public String secondName;
+            public int course;
+            public String specialty;
+            public int mathGrade;
+            public int physicsGrade;
+            public int computerScienceGrade;
+        }
+        private static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            third();
+        }
+        //firts
+        public static void first()
+        {
+            int[] b = new int[21];
+            create(b);
+            print(b);
+            Array.Sort(b);
+            print(b);
+            calculateAndReplace1D(b);
+
+        }
+
+        private static void create(int[] b)
+        {
+            for (int i = 0; i < 21; i++)
+            {
+                b[i] = rnd.Next(10, 91);
+            }
+        }
+
+        private static void calculateAndReplace1D(int[] b)
+        {
+            int sum = 0;
+            int count = 0;
+            for (int i = 0; i < 21; i++)
+            {
+                if (b[i] % 2 != 0 && b[i] % 3 == 0) continue;
+                count++;
+                sum += b[i];
+                b[i] = 0;
+            }
+            print(b);
+            Console.WriteLine(sum);
+            Console.WriteLine(count);
+        }
+
+        private static void print(int[] b) {
+            foreach (int item in b)
+            {
+                Console.Write(item.ToString()+" ");
+            }
+            Console.WriteLine();
+        }
+        //second
+        public static void second()
+        {
+            int rows = 8;
+            int cols = 5;
+            int[,] matrix = new int[rows,cols];
+            create(matrix);
+            print2D(matrix);
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            sort(matrix);
+            print2D(matrix);
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+            calculateAndReplace2D(matrix);
+        }
+
+        private static void print2D(int[,] matrix)
+        {
+            for (int i =0; i< matrix.GetLength(0);i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++) {
+                    Console.Write(matrix[i,j].ToString() + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void create(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i,j] = rnd.Next(4, 52);
+                }
+            }
+        }
+
+        private static void calculateAndReplace2D(int[,] matrix)
+        {
+            int sum = 0;
+            int count = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i,j] % 2 != 0 || (i % 3 == 0 && j % 3 == 0)) continue;
+                    count++;
+                    sum += matrix[i,j];
+                    matrix[i,j] = 0;
+                }
+            }
+            print2D(matrix);
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine(sum);
+            Console.WriteLine(count);
+        }
+
+        private static void sort(int[,] matrix)
+        {
+            for (int i0 = 0; i0 < matrix.GetLength(0) - 1; i0++)
+                for (int i1 = 0; i1 < matrix.GetLength(0) - i0 - 1; i1++)
+                    if ((matrix[i1,0] > matrix[i1 + 1,0])
+                            ||
+                            (matrix[i1,0] == matrix[i1 + 1,0] &&
+                                    matrix[i1,1] < matrix[i1 + 1,1])
+                            ||
+                            (matrix[i1,0] == matrix[i1 + 1,0] &&
+                                    matrix[i1,1] == matrix[i1 + 1,1] &&
+                                    matrix[i1,2] < matrix[i1 + 1,2]))
+                        change(matrix, i1, i1 + 1, matrix.GetLength(1));
+        }
+
+        private static void change(int[,] matrix, int row1, int row2, int colCount)
+        {
+            int tmp;
+            for (int j = 0; j < colCount; j++)
+            {
+                tmp = matrix[row1,j];
+                matrix[row1,j] = matrix[row2,j];
+                matrix[row2,j] = tmp;
+            }
+        }
+        //third
+        private static  void third()
+        {
+            StudentStruct[] students = new StudentStruct[4];
+            students[0] = new StudentStruct();
+            students[0].secondName = "Торба";
+            students[0].course = 1;
+            students[0].specialty = "Комп'ютернi науки";
+            students[0].mathGrade = 4;
+            students[0].physicsGrade = 5;
+            students[0].computerScienceGrade = 2;
+
+            students[1] = new StudentStruct();
+            students[1].secondName = "Куля";
+            students[1].course = 3;
+            students[1].specialty = "Фiзика";
+            students[1].mathGrade = 5;
+            students[1].physicsGrade = 5;
+            students[1].computerScienceGrade = 5;
+
+            students[2] = new StudentStruct();
+            students[2].secondName = "Штрик";
+            students[2].course = 2;
+            students[2].specialty = "Математики";
+            students[2].mathGrade = 3;
+            students[2].physicsGrade = 3;
+            students[2].computerScienceGrade = 3;
+
+            students[3] = new StudentStruct();
+            students[3].secondName = "Дрiт";
+            students[3].course = 1;
+            students[3].specialty = "Математики";
+            students[3].mathGrade = 3;
+            students[3].physicsGrade = 4;
+            students[3].computerScienceGrade = 4;
+            printstruct(students);
+            int superGrade = superGradeCounter(students);
+            Console.WriteLine("Students that have super grade: " + superGrade);
+
+        }
+        private static int superGradeCounter(StudentStruct[] students)
+        {
+            int counter = 0;
+            foreach (StudentStruct student in students)
+            {
+                if (student.mathGrade == 5 && student.physicsGrade == 5 && student.computerScienceGrade == 5) counter++;
+            }
+            return counter;
+        }
+        private static void printstruct(StudentStruct[] students)
+        {
+            Console.WriteLine("|{0,15:s} |{1,7:s} |{2,20:s} |{3,10:s} |{4,13:s}|{5,20:s} |", "secondName", "course", "specialty", "mathGrade", "physicsGrade", "computerScienceGrade");
+            Console.WriteLine("-------------------------------------------------------------------------------------------------");
+            foreach (StudentStruct student in students)
+            {
+                Console.WriteLine("|{0,15:s} |{1,7:d} |{2,20:s} |{3,10:d} |{4,13:d}|{5,20:d} |", student.secondName, student.course, student.specialty
+                        , student.mathGrade, student.physicsGrade, student.computerScienceGrade);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------------------------");
+        }
+
+
+    }
+}
