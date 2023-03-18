19dc80d4de989c44e8c0a7c6495586dec9d17309
Ostap Onyshkiv <prostoostap58@gmail.com>
2023-03-19 00:23:21 +0200

    added files for lab3

---
+++ lab1C/lab3/SolutionString.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace lab1C.lab3
+{
+    class SolutionString
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Input first string: ");
+        //    String s1 = Console.ReadLine();
+        //    Console.Write("Input first string: ");
+        //    String s2 = Console.ReadLine();
+
+        //    Console.WriteLine("Concatenation " + s1 + s2);
+
+        //    Console.WriteLine("s1 == s2 : " + (s1 == s2));
+        //    Console.WriteLine("s1.Equals(s2) : " + s1.Equals(s2));
+        //    Console.WriteLine("s1.CompareTo(s2) : " + s1.CompareTo(s2));
+        //    Console.WriteLine(countAndReplace(s1));
+        //    Console.ReadLine();
+
+        //}
+        public static String countAndReplace(String s) {
+            int counter = 0;
+            int i = 0;
+            while (i < s.Length - 1)
+            {
+
+                if (s[i] == ' ' && s[i + 1] == 'o')
+                {
+                    counter++;
+
+                    i++;
+                }
+                i++;
+            }
+            s = s.Replace(" o", "*");
+            Console.WriteLine(counter);
+            return s;
+        }
+    }
+}
---
+++ lab1C/lab3/SolutionStringBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1C.lab3
+{
+    class SolutionStringBuilder
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input first string: ");
+            StringBuilder s1 = new StringBuilder(Console.ReadLine());
+            Console.Write("Input first string: ");
+            StringBuilder s2 = new StringBuilder(Console.ReadLine());
+            Console.WriteLine("Concatenation " + s1 + s2);
+            Console.WriteLine("s1 == s2 : " + (s1 == s2));
+            Console.WriteLine("s1.Equals(s2) : " + s1.Equals(s2));
+            //Console.WriteLine("s1.CompareTo(s2) : " + s1.CompareTo(s2));
+            countAndReplace(s1);
+            Console.WriteLine(s1);
+            Console.ReadLine();
+
+        }
+        public static void countAndReplace(StringBuilder s)
+        {
+            int counter = 0;
+            int i = 0;
+            while (i < s.Length - 1)
+            {
+                if (s[i] == ' ' && s[i + 1] == 'o')
+                {
+                    counter++;
+
+                    i++;
+                }
+                i++;
+            }
+            s = s.Replace(" o", "*");
+            Console.WriteLine(counter);
+        }
+    }
+}
